4ceca9d88ec1e3f8c5f5b0a235c973a1b5923329
dshin0521 <dshin0521@gmail.com>
2020-02-24 23:54:19 -0800

    Added pre-order, in-order, post-order traversal & print func

--- utils/TreeUtil.java
+++ utils/TreeUtil.java
@@ -17,4 +17,25 @@ public class TreeUtil {
 
         return node;
     }
+
+    public static void printPreOrder(TreeNode root) {
+        if (root == null) return;
+        System.out.println(root.val);
+        printPreOrder(root.left);
+        printPreOrder(root.right);
+    }
+
+    public static void printInOrder(TreeNode root) {
+        if (root == null) return;
+        printInOrder(root.left);
+        System.out.println(root.val);
+        printInOrder(root.right);
+    }
+
+    public static void printPostOrder(TreeNode root) {
+        if (root == null) return;
+        printPostOrder(root.left);
+        printPostOrder(root.right);
+        System.out.println(root.val);
+    }
 }
