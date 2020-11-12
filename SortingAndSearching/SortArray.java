c7827a76fbc18b7774a420e0ce73e70d58c3484f
D Shin <dshin0521@gmail.com>
2020-11-11 23:59:32 -0800

    LC912 Sort an Array - Added bottom-up merge sort implementation

--- SortingAndSearching/SortArray.java
+++ SortingAndSearching/SortArray.java
@@ -27,12 +27,12 @@ public class SortArray {
     /**
      * Top-down merge sort
      */
-    public static int[] sortArray(int[] nums) {
+    public static int[] mergeSortTopDown(int[] nums) {
         if (nums.length <= 1) return nums;
         int midIdx = nums.length / 2;
 
-        int[] leftArr = sortArray(Arrays.copyOfRange(nums, 0, midIdx));
-        int[] rightArr = sortArray(Arrays.copyOfRange(nums, midIdx, nums.length));
+        int[] leftArr = mergeSortTopDown(Arrays.copyOfRange(nums, 0, midIdx));
+        int[] rightArr = mergeSortTopDown(Arrays.copyOfRange(nums, midIdx, nums.length));
 
         return merge(leftArr, rightArr);
     }
@@ -59,7 +59,55 @@ public class SortArray {
         return mergedArr;
     }
 
+    /**
+     * Bottom-up merge sort
+     */
+    public static int[] mergeSortBottomUp(int[] nums) {
+        if (nums.length <= 1) return nums;
+
+        int[] numsCopy = Arrays.copyOf(nums, nums.length);
+
+        // Divide the array into blocks of size i
+        // i = [1, 2, 4, 8, 16 ...]
+        for (int i = 1; i < nums.length; i *= 2) {
+
+            // Start index of each block with size of i
+            // i = 1, j = 0, 2, 4, 6, 8 ...
+            // i = 2, j = 0, 4, 8, 12, 24 ...
+            // i = 4, j = 0, 8, 16, 32, 64 ...
+            for (int j = 0; j < nums.length - 1; j += 2 * i) {
+                int fromIdx = j;
+                int midIdx = j + i - 1;
+                int toIdx = Math.min(j + 2 * i - 1, nums.length - 1);
+                mergeSortBottomUp(nums, numsCopy, fromIdx, midIdx, toIdx);
+            }
+        }
+        return nums;
+    }
+
+    private static void mergeSortBottomUp(int[] originalArr, int[] copyArr, int fromIdx, int midIdx, int toIdx) {
+
+        int i = fromIdx, j = midIdx + 1, k = fromIdx;
+
+        // Loop till there are elements in both left and right blocks
+        while (i <= midIdx && j <= toIdx) {
+            if (originalArr[i] < originalArr[j]) copyArr[k++] = originalArr[i++];
+            else copyArr[k++] = originalArr[j++];
+        }
+
+        // Copy remaining elements
+        while (i <= midIdx) copyArr[k++] = originalArr[i++];
+
+        // Copy back to the original array to reflect sorted order
+        for (i = fromIdx; i < toIdx; i++) {
+            originalArr[i] = copyArr[i];
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        ArrayUtil.printIntArray(sortArray(new int[]{1,5,3,2,8,7,6,4}));
+        ArrayUtil.printIntArray(mergeSortTopDown(new int[]{1,5,3,2,8,7,6,4}));
+        ArrayUtil.printIntArray(mergeSortBottomUp(new int[]{1,5,3,2,8,7,6,4}));
     }
 }
