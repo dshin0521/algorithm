fa621d23065def67ae678ec20a596fb731fcd446
D Shin <dshin0521@gmail.com>
2020-11-13 16:41:10 -0800

    LC912 Sort an Array - Added quicksort implementation

--- SortingAndSearching/SortArray.java
+++ SortingAndSearching/SortArray.java
@@ -106,10 +106,36 @@ public class SortArray {
         }
     }
 
+    /**
+     * quick sort
+     */
+    public static int[] quickSort(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
 
+    public static void quickSort(int[] nums, int l, int r) {
+        if (l >= r) return;
+        int mid = partition(nums, l, r);
+        quickSort(nums, l, mid);
+        quickSort(nums, mid+1, r);
+    }
+
+    private static int partition(int[] nums, int l, int r) {
+        int pivot = nums[l];
+        while (l < r) {
+            while (l < r && nums[r] >= pivot) r--;
+            nums[l] = nums[r];
+            while (l < r && nums[l] <= pivot) l++;
+            nums[r] = nums[l];
+        }
+        nums[l] = pivot;
+        return l;
+    }
 
     public static void main(String[] args) {
         ArrayUtil.printIntArray(mergeSortTopDown(new int[]{1,5,3,2,8,7,6,4}));
         ArrayUtil.printIntArray(mergeSortBottomUp(new int[]{5,3,2,8,7,6,4,1}));
+        ArrayUtil.printIntArray(quickSort(new int[]{5,3,2,8,7,6,4,1}));
     }
 }
