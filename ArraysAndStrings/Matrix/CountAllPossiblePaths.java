f59fac9e29e6974d29a01ab7bb63ab87e75b608d
D Shin <dshin0521@gmail.com>
2020-11-18 14:11:26 -0800

    Count all possible path from a matrix - Added iterative solution

--- ArraysAndStrings/Matrix/CountAllPossiblePaths.java
+++ ArraysAndStrings/Matrix/CountAllPossiblePaths.java
@@ -34,29 +34,47 @@ package ArraysAndStrings.Matrix;
  * Third path is from index (0, 0) to (0, 1) and (0, 1) to (1, 1) and (1, 1) to (1, 2) and (1, 2) to (2, 2)
  */
 public class CountAllPossiblePaths {
-    public static int countPath(int[][] matrix) {
-        return countPath(matrix, 0, 0);
+    public static int countPathRecursive(int[][] matrix) {
+        return countPathRecursive(matrix, 0, 0);
     }
 
-    private static int countPath(int[][] matrix, int row, int col) {
+    private static int countPathRecursive(int[][] matrix, int row, int col) {
         if (row == matrix.length - 1 && col == matrix[0].length - 1) return 1;
 
-        int pathToRigth = 0, pathToDown = 0;
+        int pathToRight = 0, pathToDown = 0;
         if (row < matrix.length - 1 && matrix[row + 1][col] != -1) {
-            pathToRigth = countPath(matrix, row + 1, col);
+            pathToRight = countPathRecursive(matrix, row + 1, col);
         }
         if (col < matrix[0].length - 1 && matrix[row][col + 1] != -1) {
-            pathToDown = countPath(matrix, row, col + 1);
+            pathToDown = countPathRecursive(matrix, row, col + 1);
         }
 
-        return pathToRigth + pathToDown;
+        return pathToRight + pathToDown;
+    }
+
+    public static int countPathIterative(int[][] matrix) {
+        int[][] pathCount = new int[matrix.length][matrix[0].length];
+        pathCount[0][0] = 1;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == -1) pathCount[i][j] = 0;
+                else {
+                    if (i - 1 >= 0) pathCount[i][j] += pathCount[i - 1][j];
+                    if (j - 1 >= 0) pathCount[i][j] += pathCount[i][j - 1];
+                }
+            }
+        }
+        return pathCount[matrix.length - 1][matrix[0].length - 1];
     }
 
     public static void main(String[] args) {
         int[][] testMatrix = new int[][]{{1,1,1},{1,-1,1},{1,1,1}};
-        System.out.println(countPath(testMatrix));
+        System.out.println(countPathRecursive(testMatrix));
+        System.out.println(countPathIterative(testMatrix));
 
         testMatrix = new int[][]{{1,1,1},{1,1,1},{1,-1,1}};
-        System.out.println(countPath(testMatrix));
+        System.out.println(countPathRecursive(testMatrix));
+        System.out.println(countPathIterative(testMatrix));
     }
 }
