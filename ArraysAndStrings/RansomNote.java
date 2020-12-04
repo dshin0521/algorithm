763aa765ec5dc592aa8a8fc853dc158f4dde08bd
D Shin <dshin0521@gmail.com>
2020-12-04 14:41:32 -0800

    LC383 Ransom Note

---
+++ ArraysAndStrings/RansomNote.java
@@ -0,0 +1,48 @@
+package ArraysAndStrings;
+
+/**
+ * Given an arbitrary ransom note string and another string containing letters from all the magazines,
+ * write a function that will return true if the ransom note can be constructed from the magazines.
+ * Otherwise, it will return false.
+ * Each letter in the magazine string can only be used once in your ransom note.
+ *
+ * Example 1:
+ *
+ * Input: ransomNote = "a", magazine = "b"
+ * Output: false
+ *
+ * Example 2:
+ *
+ * Input: ransomNote = "aa", magazine = "ab"
+ * Output: false
+ *
+ * Example 3:
+ *
+ * Input: ransomNote = "aa", magazine = "aab"
+ * Output: true
+ *
+ * Constraints:
+ *
+ * You may assume that both strings contain only lowercase letters.
+ */
+public class RansomNote {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        if (magazine == null) return false;
+
+        int[] charCountArr = new int[26];
+
+        for (int i = 0; i < magazine.length(); i++) {
+            charCountArr[magazine.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (--charCountArr[ransomNote.charAt(i) - 'a'] < 0) return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canConstruct("aa", "aab"));
+    }
+}
